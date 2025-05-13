52ebec313ca8d44390126f054d44728a8d9efc09
Oscar-Ruan <oscarr193@nycstudents.net>
2025-05-13 09:23:44 -0400

    im so lost....

--- app.py
+++ app.py
@@ -2,9 +2,32 @@ from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
+id = 3
+
+def pokemon_detail(id):
+    response = requests.get(f"https://www.fruityvice.com/api/fruit/{id}")
+    fruit_details = response.json()
+    for fruit in fruit_details:
+        print(fruit)
 
 @app.route("/")
 def index():
     response = requests.get("https://www.fruityvice.com/api/fruit/all")
-    data = response.json()
-    fruits_list = data['results']
+    fruit_list = response.json()
+
+    fruits = []
+
+    for fruit in fruit_list:
+        fruits.append({
+            'name': fruit["name"].capitalize(),
+            'id': fruit["id"]
+        })
+
+    return render_template("index.html", fruits=fruits)
+
+@app.route("/pokemon/<int:id>")
+def pokemon_detail(id):
+    response = requests.get(f"https://www.fruityvice.com/api/fruit/{id}")
+    fruit_details = response.json()
+    for fruit in fruit_details:
+        print(fruit)
---
+++ notes.py
@@ -0,0 +1,8 @@
+def divide(a,b):
+    try:
+        #try something
+        result = a/b
+    except ZeroDivisionError:
+        print("Error cannot divide by 0")
+    else:
+        print(result)
